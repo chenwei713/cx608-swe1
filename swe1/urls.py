01b4f4383adc55e96294eba7546173e592f7e26c
chenwei713 <chenwei713@foxmail.com>
2020-10-21 14:28:01 -0400

    Travis

--- swe1/urls.py
+++ swe1/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('helloworld/', include('helloworld.urls')),
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
+    path("helloworld/", include("helloworld.urls")),
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
 ]
